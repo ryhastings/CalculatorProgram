8b71b599b566eb90e1abb42b5f0c14f7d85bea75
Ryan Hastings <rjh53@duke.edu>
2019-09-09 12:23:12 -0400

    wrote comparison function to give readout

--- calculator.py
+++ calculator.py
@@ -1,3 +1,10 @@
+def check_HDL(HDL_result):
+	if HDL_result >= 60:
+		return "Normal"
+	elif 40 <= HDL < 60:
+		return "Borderline low"
+	else:
+		return "Low"
 def interface():
 	print("My calculator program")
 	keep_running = True
