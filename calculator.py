8771806b5a7205f3f1c47b1146a7bc8dab0e62d0
Ryan Hastings <rjh53@duke.edu>
2019-09-09 12:04:57 -0400

    design template interface

---
+++ calculator.py
@@ -0,0 +1,9 @@
+def interface():
+	print("My calculator program")
+	print("Option: ")
+	print("9 - Quit")
+	choice = input("Enter your choice")
+	if choice == '9':
+			return
+if __name__ == "__main__":
+	interface()
